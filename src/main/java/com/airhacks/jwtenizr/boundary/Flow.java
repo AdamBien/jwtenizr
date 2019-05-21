6402414f98cb23426dca8375dbe27e69f143e1a5
Adam Bien <abien@adam-bien.com>
2019-05-21 21:44:55 +0200

    key reuse implemented

--- src/main/java/com/airhacks/jwtenizr/boundary/Flow.java
+++ src/main/java/com/airhacks/jwtenizr/boundary/Flow.java
@@ -5,31 +5,36 @@ import com.airhacks.jwtenizr.control.FileManager;
 import com.airhacks.jwtenizr.control.JwtTokenGenerator;
 import com.airhacks.jwtenizr.control.KeyGenerator;
 import com.airhacks.jwtenizr.control.Terminal;
-import java.io.IOException;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import javax.json.JsonObject;
 
 public interface Flow {
 
     static final String KEY_FILE_NAME = "jwtenizr";
     static final String PRIVATE_KEY_FILE_NAME = KEY_FILE_NAME + ".key";
+    static final String PUBLIC_KEY_FILE_NAME = KEY_FILE_NAME + ".pub";
     static final String TOKEN_FILE_NAME = "jwt-token.json";
     static final String TOKEN_FILE = "token.jwt";
 
-    static void establishPreconditions() throws IOException {
-        if (!Files.exists(Paths.get(TOKEN_FILE_NAME))) {
-            Terminal.info(TOKEN_FILE_NAME + " does not exist");
+    static void establishPreconditions() throws Exception {
+        if (FileManager.exists(KEY_FILE_NAME)) {
+            Terminal.info(TOKEN_FILE_NAME + " does not exist, generating default one");
             JsonObject defaultToken = JwtTokenGenerator.createDefaultToken();
             FileManager.write(TOKEN_FILE_NAME, defaultToken);
+        } else {
+            Terminal.info("Token template " + TOKEN_FILE + " is used");
+        }
+        if (!FileManager.exists(PRIVATE_KEY_FILE_NAME) || !FileManager.exists(PUBLIC_KEY_FILE_NAME)) {
+            Terminal.info("public or private key not exist -> generate new");
+            KeyGenerator generator = new KeyGenerator(KEY_FILE_NAME);
+            generator.generateKeys();
+        } else {
+            Terminal.info(PRIVATE_KEY_FILE_NAME + " " + PUBLIC_KEY_FILE_NAME + " are reused");
         }
     }
 
     public static void generateToken() throws Exception {
         establishPreconditions();
-        KeyGenerator generator = new KeyGenerator(KEY_FILE_NAME);
-        generator.generateKeys();
         String jwtToken = JwtTokenGenerator.generateJWTString(TOKEN_FILE_NAME, PRIVATE_KEY_FILE_NAME);
         Terminal.info(jwtToken);
         FileManager.writeBytes(TOKEN_FILE, jwtToken.getBytes(Charset.defaultCharset()));
