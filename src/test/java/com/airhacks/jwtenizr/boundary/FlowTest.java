2d8976b75625a415e6930e559093883370dfba34
Adam Bien <abien@adam-bien.com>
2019-05-21 16:33:05 +0200

    flow test introduced

---
+++ src/test/java/com/airhacks/jwtenizr/boundary/FlowTest.java
@@ -0,0 +1,18 @@
+/*
+ */
+package com.airhacks.jwtenizr.boundary;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class FlowTest {
+
+    @Test
+    public void testGeneratedToken() throws Exception {
+        Flow.generateToken();
+    }
+
+}
