9aeb601c52fc55d05f1bc4bb73713ac2b8211554
Adam Bien <abien@adam-bien.com>
2019-05-21 16:32:51 +0200

    method name fixed

---
+++ src/main/java/jwtenizr/App.java
@@ -0,0 +1,19 @@
+
+package jwtenizr;
+
+import com.airhacks.jwtenizr.boundary.Flow;
+import com.airhacks.jwtenizr.control.Terminal;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class App {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException, Exception {
+        Flow.generateToken();
+        Terminal.info("---");
+    }
+}
