53188e714c4e33776f22f6b280e20b59d3c78dc4
Adam Bien <abien@adam-bien.com>
2019-05-21 16:22:29 +0200

    Terminal introduced for console output

---
+++ src/main/java/com/airhacks/jwtenizr/control/Terminal.java
@@ -0,0 +1,13 @@
+
+package com.airhacks.jwtenizr.control;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public interface Terminal {
+
+    public static void info(String message) {
+        System.out.println(message);
+    }
+}
