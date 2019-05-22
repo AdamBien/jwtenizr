916716db3f9966381d2fc511aee795a1fa03833f
Adam Bien <abien@adam-bien.com>
2019-05-22 09:43:16 +0200

    private key loaded from configuration for jwt generation

--- src/main/java/com/airhacks/jwtenizr/boundary/Flow.java
+++ src/main/java/com/airhacks/jwtenizr/boundary/Flow.java
@@ -1,6 +1,7 @@
 
 package com.airhacks.jwtenizr.boundary;
 
+import com.airhacks.jwtenizr.control.Configuration;
 import com.airhacks.jwtenizr.control.Curl;
 import com.airhacks.jwtenizr.control.FileManager;
 import com.airhacks.jwtenizr.control.JwtTokenGenerator;
@@ -11,26 +12,24 @@ import javax.json.JsonObject;
 
 public interface Flow {
 
-    static final String KEY_FILE_NAME = "jwtenizr";
-    static final String PRIVATE_KEY_FILE_NAME = KEY_FILE_NAME + ".key";
-    static final String PUBLIC_KEY_FILE_NAME = KEY_FILE_NAME + ".pub";
     static final String TOKEN_FILE_NAME = "jwt-token.json";
     static final String TOKEN_FILE = "token.jwt";
 
     static void establishPreconditions() throws Exception {
-        if (FileManager.exists(KEY_FILE_NAME)) {
-            Terminal.info(TOKEN_FILE_NAME + " does not exist, generating default one");
+
+        if (FileManager.exists(TOKEN_FILE_NAME)) {
+            Terminal.info(TOKEN_FILE_NAME + " does not exist, generating default token template");
             JsonObject defaultToken = JwtTokenGenerator.createDefaultToken();
             FileManager.write(TOKEN_FILE_NAME, defaultToken);
         } else {
             Terminal.info("Token template " + TOKEN_FILE + " is used");
         }
-        if (!FileManager.exists(PRIVATE_KEY_FILE_NAME) || !FileManager.exists(PUBLIC_KEY_FILE_NAME)) {
+
+        if (!Configuration.keysExist()) {
             Terminal.info("public or private key not exist -> generate new");
-            KeyGenerator generator = new KeyGenerator(KEY_FILE_NAME);
-            generator.generateKeys();
+            KeyGenerator.generateKeys();
         } else {
-            Terminal.info(PRIVATE_KEY_FILE_NAME + " " + PUBLIC_KEY_FILE_NAME + " are reused");
+            Terminal.info("Keys from are reused from the configuration file");
         }
     }
 
@@ -40,7 +39,8 @@ public interface Flow {
 
         }
         establishPreconditions();
-        String jwtToken = JwtTokenGenerator.generateJWTString(TOKEN_FILE_NAME, PRIVATE_KEY_FILE_NAME);
+        String privateKey = Configuration.loadPrivateKey();
+        String jwtToken = JwtTokenGenerator.generateJWTString(TOKEN_FILE_NAME, privateKey);
         Terminal.info("---jwt---");
         Terminal.info(jwtToken);
         Terminal.info("---------");
