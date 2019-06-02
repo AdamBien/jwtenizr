ab72805eb08345b5fd95fca5653a6e07d74210e9
Adam Bien <abien@adam-bien.com>
2019-06-02 10:36:33 +0200

    time output implemented

--- src/main/java/com/airhacks/jwtenizr/control/Terminal.java
+++ src/main/java/com/airhacks/jwtenizr/control/Terminal.java
@@ -1,13 +1,24 @@
 
 package com.airhacks.jwtenizr.control;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import static java.time.temporal.ChronoField.HOUR_OF_DAY;
+import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
+import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
+import java.util.Properties;
+
 /**
  *
  * @author airhacks.com
  */
 public interface Terminal {
 
-    public static void userInfo(String message) {
+    public static void printUserInfo(String message) {
         System.out.println(message);
     }
 
@@ -29,4 +40,48 @@ public interface Terminal {
     public static boolean isVerbose() {
         return System.getProperties().containsKey("verbose");
     }
+
+    static void printTime() {
+        System.out.printf("[%s%s%s]", TerminalColors.TIME.value(), currentFormattedTime(), TerminalColors.RESET.value());
+    }
+
+    public static void printWelcomeMessage() throws IOException {
+        try (InputStream resourceAsStream = Terminal.class.
+                getClassLoader().
+                getResourceAsStream("META-INF/maven/com.airhacks/wad/pom.properties")) {
+            Properties properties = new Properties();
+            properties.load(resourceAsStream);
+            String wad = properties.getProperty("artifactId");
+            String version = properties.getProperty("version");
+            printTime();
+            System.out.println(wad + " " + version);
+        }
+    }
+
+    static void printDoneMessage() {
+        printTime();
+        System.out.println("----");
+        System.out.println("JWT generated");
+    }
+
+
+    static String currentFormattedTime() {
+        DateTimeFormatter timeFormatter = new DateTimeFormatterBuilder()
+                .appendValue(HOUR_OF_DAY, 2)
+                .appendLiteral(':')
+                .appendValue(MINUTE_OF_HOUR, 2)
+                .optionalStart()
+                .appendLiteral(':')
+                .appendValue(SECOND_OF_MINUTE, 2)
+                .appendLiteral(':')
+                .appendValue(ChronoField.MILLI_OF_SECOND, 2)
+                .toFormatter();
+
+        return LocalTime.now().format(timeFormatter);
+    }
+
+    public static void printCommand(String command) {
+        printUserInfo(command);
+    }
+
 }
